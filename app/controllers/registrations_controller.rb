791c99ab39909b77eead8006e2e9b07c30824df0
Miguel Cabero <lemig@hotmail.com>
2015-03-20 13:58:22 +0100

    User can change email, password and delete account

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,4 @@
 class RegistrationsController < Devise::RegistrationsController
-
-  protected
-
-  def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
- 
   private
  
   def sign_up_params
