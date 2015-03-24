3ff35d5a5d851dbc89fc1ceaca7626b3b99d9070
Camilla van Klinken <camillavk@gmail.com>
2015-03-24 09:51:14 +0100

    removed unneeded comments

--- db/schema.rb
+++ db/schema.rb
@@ -14,9 +14,6 @@
 
 ActiveRecord::Schema.define(version: 20150320140924) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "answers", force: :cascade do |t|
     t.text     "content"
     t.integer  "marks"
