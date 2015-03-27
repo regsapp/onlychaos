7c3d72da673fce427dcd6f04f9fb7da565128cc2
Camilla van Klinken <camillavk@gmail.com>
2015-03-27 15:07:02 +0100

    changed title of edit account page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    current_user.student? ? dashboard_path : users_path
+    current_user.student? ? dashboard_path : root_path
   end
 end
