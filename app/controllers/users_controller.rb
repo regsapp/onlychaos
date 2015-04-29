7ddcfabadee17605097b13365480f40d221a8500
Camilla van Klinken <camillavk@gmail.com>
2015-04-29 11:42:30 +0100

    stlyed password reset pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,8 +30,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        format.html { redirect_to users_url, notice: 'User was successfully created.' }
+        # format.json { render :show, status: :created, location: @user }
+        format.json { head :no_content }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
