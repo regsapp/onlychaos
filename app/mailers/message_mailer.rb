277f237ab9371dcd93ca3e920fe6a7cf3988490a
Camilla van Klinken <camillavk@gmail.com>
2015-04-30 11:48:03 +0100

    corrected email address

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,9 +1,9 @@
 class MessageMailer < ApplicationMailer
   default from: "no-reply@gradezilla.com"
-  default to: "camilla@gmail.com"
+  default to: "camillavk@gmail.com"
 
   def new_message(message)
     @message = message
-    mail(subject: "New Message: #{message.title}")
+    mail(subject: "#{message.title}")
   end
 end
