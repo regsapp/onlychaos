f2060b2db97d4be07bc990bd3ddabc76365b1a7b
Camilla van Klinken <camillavk@gmail.com>
2015-05-05 13:11:46 +0100

    No of questions for test view changed to available marks

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -12,4 +12,13 @@ module TestsHelper
     test.incorrect_answers_count.to_f / total_answers_count(test).to_f * 100
   end
 
+  def total_marks_available(test)
+    questions = test.questions
+    array_marks = []
+    questions.each do |question|
+      array_marks << question.marks
+    end
+    array_marks.sum
+  end
+
 end
