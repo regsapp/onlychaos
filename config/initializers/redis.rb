76e86306441b66e46a5dea9fc98aac615bca87f2
Camilla van Klinken <camillavk@gmail.com>
2015-05-11 11:10:46 +0100

    changed redis to specify environment

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,12 @@
-uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379"
+if Rails.env.production?
+  uri = ENV["REDISTOGO_URL"]
+else
+  uri = "redis://localhost:6379"
+end
 Redis.current = Redis.new(:url => uri)
+
+# REDIS_CONFIG = YAML.load_file(Rails.root.join('config', 'redis.yml'))[Rails.env]
+# Redis.current = Redis.new(:driver => :hiredis,
+#                           :host => REDIS_CONFIG['host'],
+#                           :port => REDIS_CONFIG['port'],
+#                           :db => REDIS_CONFIG['database'])
