50747f577b2c6f7cc47d851691d4ec59c091c1f7
Camilla van Klinken <camillavk@gmail.com>
2015-05-12 13:06:40 +0100

    change redis.rb according to redis cloud suggestion

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,7 +3,7 @@
 # else
 #   uri = "redis://localhost:6379"
 # end
-uri = ENV["REDISCLOUD_URL"]
+uri = ENV[ENV["REDIS_PROVIDER"]]
 Redis.current = Redis.new(:url => uri)
 
 # REDIS_CONFIG = YAML.load_file(Rails.root.join('config', 'redis.yml'))[Rails.env]
